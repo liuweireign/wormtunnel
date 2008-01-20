b763a97c5eeea90ec0242973becca0c65b981478
vpnhacker <vpnhacker@896d8a4f-2b44-0410-9c49-79501efe3a37>
2008-01-20 19:28:36 +0000

    写了一点http通道。今天没空了，还要看QRMonitor阿。

--- trunk/vpnclient/httptunnel.py
+++ trunk/vpnclient/httptunnel.py
@@ -10,3 +10,9 @@ HTTP通道，提供上传以太网包和获取以太网包的功能
 import urllib
 import urllib2
 import httplib
+
+uploadpacket=lambda pkt:urllib2.urlopen(settings.VPN_SERVER_URL_UPLOAD,pkt)
+
+def downloadpackets():
+    """下载回来一批另外一端上传的包"""
+    return
