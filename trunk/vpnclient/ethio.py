326c0d55b2723d3e3c71fec72138a57d0d99cd0d
vpnhacker <vpnhacker@896d8a4f-2b44-0410-9c49-79501efe3a37>
2008-01-20 19:08:43 +0000

    ethio模块基本完善了，准备开发下一个模块。

--- trunk/vpnclient/ethio.py
+++ trunk/vpnclient/ethio.py
@@ -24,16 +24,15 @@ class CAPThread(threading.Thread):
         self.pc=pcap.pcap(settings.ETHIF)
         self.pc.setnonblock(True)
         self.pc.setfilter(settings.PCAP_FILTER)
-        selt.setDaemon(True)
+        self.setDaemon(True)
         return
 
     def run(self):
         while self.looping:
-            pkt=self.pc.next()
-            print pkt
+            tm,pkt=self.pc.next()
             pkt=str(pkt)
             if self.filterpacket(pkt):
-                self.pktqueue.add(pkt)
+                self.pktqueue.put(pkt)
         return
 
     def filterpacket(self,pkt):
@@ -43,8 +42,12 @@ class CAPThread(threading.Thread):
         self.looping=False
         return
 
+sendpacket=lambda pkt:sendpkt.sendpacket(pkt,settings.ETHIF)
+
 ###############################################################################
 import unittest
+import Queue
+import time
 
 class TestCapThread(unittest.TestCase):
 
@@ -53,3 +56,16 @@ class TestCapThread(unittest.TestCase):
 
     def tearDown(self):
         return
+
+    def testall_1(self):
+        q=Queue.Queue()
+        thrd=CAPThread(q)
+        thrd.start()
+        time.sleep(5)
+        thrd.stop()
+        for i in range(0,min(q.qsize(),10)):
+            print repr(q.get())
+        return
+
+if __name__=='__main__':
+    unittest.main()
