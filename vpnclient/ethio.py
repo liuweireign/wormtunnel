fb4b2d9aff6791dbf034b2ef0d8569c14bd45747
vpnhacker <vpnhacker@896d8a4f-2b44-0410-9c49-79501efe3a37>
2008-01-20 18:56:19 +0000

    加入了配置文件，把ethio.py写了一个基本完整的框架了。

--- vpnclient/ethio.py
+++ vpnclient/ethio.py
@@ -7,5 +7,49 @@
 以太网抓包和发包接口
 """
 
+import threading
+
 import pcap
 import sendpkt
+
+import settings
+
+class CAPThread(threading.Thread):
+    """抓包线程"""
+
+    def __init__(self,pktqueue):
+        threading.Thread.__init__(self)
+        self.pktqueue=pktqueue
+        self.looping=True
+        self.pc=pcap.pcap(settings.ETHIF)
+        self.pc.setnonblock(True)
+        self.pc.setfilter(settings.PCAP_FILTER)
+        selt.setDaemon(True)
+        return
+
+    def run(self):
+        while self.looping:
+            pkt=self.pc.next()
+            print pkt
+            pkt=str(pkt)
+            if self.filterpacket(pkt):
+                self.pktqueue.add(pkt)
+        return
+
+    def filterpacket(self,pkt):
+        return True
+
+    def stop(self):
+        self.looping=False
+        return
+
+###############################################################################
+import unittest
+
+class TestCapThread(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
