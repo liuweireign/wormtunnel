899087f7245b2563e2f104e43d75e0e63afc7aae
vpnhacker <vpnhacker@896d8a4f-2b44-0410-9c49-79501efe3a37>
2008-01-20 18:56:19 +0000

    加入了配置文件，把ethio.py写了一个基本完整的框架了。

---
+++ trunk/vpnclient/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# File: settings.py
+# Date: 2008-01-21
+# Author: vpnhacker
+
+"""
+vpn客户端配置文件
+"""
+
+VPN_SERVER_URL='http://localhost:3344/ethpkt'
+ETHIF='eth1'
+PCAP_FILTER=''
